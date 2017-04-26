835406e8500aaf9aa5ad3bfa77b9a438787fc2da
Alyssa Moody and Chase Grijalva <chase@>
2017-04-26 10:20:47 -0700

    Add forms for subtract, multiply and divide and answer section for each.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ var divide = function(number1, number2) {
 	return number1 / number2;
 };
 
-// Everything below this line is user interface (or front-end) logic:
+// User interface (or front-end) logic:
 
 $(document).ready(function() {
 	$("form#add").submit(function(event) {
@@ -24,6 +24,27 @@ $(document).ready(function() {
 		var number1 = parseInt($("#add1").val());
 	  var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-		$("#output").text(result);
+		$("#output-add").text(result);
+	});
+	$("form#subtract").submit(function(event) {
+	 event.preventDefault();
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		var result = subtract(number1, number2);
+		$("#output-sub").text(result);
+	});
+	$("form#multiply").submit(function(event) {
+	 event.preventDefault();
+		var number1 = parseInt($("#mult1").val());
+		var number2 = parseInt($("#mult2").val());
+		var result = multiply(number1, number2);
+		$("#output-mult").text(result);
+	});
+	$("form#divide").submit(function(event) {
+	 event.preventDefault();
+		var number1 = parseInt($("#div1").val());
+		var number2 = parseInt($("#div2").val());
+		var result = divide(number1, number2);
+		$("#output-div").text(result);
 	});
 });
